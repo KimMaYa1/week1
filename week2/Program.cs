91b29bf335e6d28d78a4851bf553f217eaa48758
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-08-14 20:10:26 +0900

    1, 2 주차 숙제 끝

--- week2/Program.cs
+++ week2/Program.cs
@@ -110,45 +110,21 @@ namespace week2
 
         static int CheckWin()
         {
-            /*for(int i = 0; i > 9; i+=3) //가로승리
+            for(int i = 0; i < 9; i+=3) //가로승리
             {
                 if (arr[i] == arr[i+1] && arr[i+1] == arr[i+2])
                 {
                     return 1;
                 }
             }
-            for(int i = 0; i > 3; i++)  //세로승리
+            for(int i = 0; i < 3; i++)  //세로승리
             {
                 if (arr[i] == arr[i+3] && arr[i+3] == arr[i + 6])
                 {
                     return 1;
                 }
-            }*/
-            if (arr[0] == arr[1] && arr[1] == arr[2])
-            {
-                return 1;
             }
-            else if (arr[3] == arr[4] && arr[4] == arr[5])
-            {
-                return 1;
-            }
-            else if (arr[6] == arr[7] && arr[7] == arr[8])
-            {
-                return 1;
-            }
-            else if (arr[0] == arr[3] && arr[3] == arr[6])
-            {
-                return 1;
-            }
-            else if (arr[1] == arr[4] && arr[4] == arr[7])
-            {
-                return 1;
-            }
-            else if (arr[2] == arr[5] && arr[5] == arr[8])
-            {
-                return 1;
-            }
-            else if (arr[0] == arr[4] && arr[0] == arr[8] || arr[2] == arr[4] && arr[2] == arr[6])
+            if (arr[0] == arr[4] && arr[0] == arr[8] || arr[2] == arr[4] && arr[2] == arr[6])
             {
                 return 1;
             }
